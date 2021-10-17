56376e53c4c799a379a79c7b25697d3d3f0adb1c
agridnev228 <agridnev228@gmail.com>
2021-10-17 19:30:26 +0500

    task01

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,6 +2,11 @@ package com.example.task01;
 
 public class Task01Main {
     public static void main(String[] args) {
-
+        Logger logger1 = new Logger("log1");
+        Logger logger2 = new Logger("log2");
+        logger1.setLevel(Level.WARNING);
+        logger2.setLevel(Level.DEBUG);
+        logger1.info("low level test");
+        logger2.error("up level test");
     }
 }
