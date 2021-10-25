ccabb5d4713c48c5a524e249f96b0f84e035fbb5
EstSov <fedorovama@gmail.com>
2021-10-25 16:28:33 +0500

    task03

--- task03/src/com/example/task03/Hours.java
+++ task03/src/com/example/task03/Hours.java
@@ -5,7 +5,10 @@ public class Hours implements TimeUnit{
     private final long amount;
 
     public Hours(long amount) {
-        this.amount = amount;
+        if(amount<0)
+        {
+            throw new IllegalArgumentException("Hours less then zero");
+        }this.amount = amount;
     }
 
     @Override
