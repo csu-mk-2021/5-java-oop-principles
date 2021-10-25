75784c6f9986ba7f7544c71f604c7c1e84cef519
MagTemTenebrius <magtemtenebrius@gmail.com>
2021-10-25 16:08:28 +0500

    hot fix

--- task02/src/com/example/task02/DiscountBill.java
+++ task02/src/com/example/task02/DiscountBill.java
@@ -1,7 +1,18 @@
 package com.example.task02;
 
 public class DiscountBill extends Bill {
-    private double discount = 0;
+    private double discount;
+
+    /**
+     * Конструктор списка покупок со скидкой
+     *
+     * @param discount [0...100] - скидка в процентах
+     */
+    public DiscountBill(double discount) {
+        if (discount < 0 || discount > 100)
+            throw new IllegalArgumentException("discount = [0, 100]");
+        this.discount = discount;
+    }
 
     public double getDiscount() {
         return discount;
