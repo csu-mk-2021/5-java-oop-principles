d1a951462fb68f7de29c481fee7c437ba7ee9090
blackrose198 <grushinskayan@gmail.com>
2021-10-25 17:15:57 +0500

    task4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,9 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
-
+        Logger logger = new Logger("test", new FileHandler("test.log"), Level.DEBUG);
+        logger.info("test1");
+        logger.info("test2");
+        logger.info("test3");
     }
 }
